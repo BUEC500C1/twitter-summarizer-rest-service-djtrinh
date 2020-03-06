f3f2816780213eeb9c0b4e239e3ef9394ab251de
Danny Trinh <djtrinh@bu.edu>
2020-03-06 13:09:36 -0500

    Restful updates

--- main.py
+++ main.py
@@ -7,22 +7,76 @@ import os.path
 import media_creator
 import twitter_api as twit
 import datetime
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, request
+from flask_restful import reqparse, abort, Api, Resource
 
 
 app = Flask(__name__)
 api = Api(app)
 
-
 # create queue
 q1 = queue.Queue(maxsize=4)
 q2 = queue.Queue()
 
+TODOS = {
+    'user': {'id': 'Generate images and video based off provided id'},
+}
 
-@app.route("/")
-def home():
-    return "Test"
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+class video_restful(Resource):
+    def get(self, todo_id):
+        return TODOS
+
+    def put(self, todo_id):
+        username = request.form['user']
+        if username != '':
+            # Remove old pictures with matching Twitter ID
+            filelist = glob.glob(os.path.join(r'processed_imgs/', username + "*.png"))
+            if len(filelist) > 0:
+                for f in filelist:
+                    os.remove(f)
+            # Create processes to start generating pictures
+            q_item = [username, twit.get_user_pic(username), twit.get_users_tweets(username)]
+            t = threading.Thread(name="ProducerThread", target=producer, args=(q1, q_item))
+            date_time = str(datetime.date.today()).replace('-', '_')
+            q2.put([username, date_time])
+            t.start()
+            return "Running video creater with final video at 3.19.22.162/video/twitter_feed_" + username + '_' + date_time + '.mp4', 200
+        else:
+            return "Please enter a valid ID", 400
+
+
+class home(Resource):
+    def get(self):
+        return {"message": "Twitter to Video Page"}
+
+
+# Multimedia playback is better to not be restful
+@app.route("/video/<name>")
+def play_video(name):
+    if not os.path.isfile('./static/' + name):
+        return "Video is still processing", 400
+    html = f"""
+        <center>
+            <video width="768" controls>
+                <source src="/static/{name}" type="video/mp4">
+            </video>
+        </center>
+    """
+    return html, 200
+
+
+api.add_resource(video_restful, '/user/<todo_id>')
+api.add_resource(home, '/')
 
 
 # Thread that processes create image requests. 4 of these are run
@@ -68,43 +122,6 @@ def producer(q, q_item):
     for count, tweet in enumerate(q_item[2]):
         q.put([q_item[0], q_item[1], tweet, count])
     q.join()
-
-
-# function that serves the user
-@app.route('/user/<username>')
-def server(username):
-    if username != '':
-        # Remove old pictures with matching Twitter ID
-        filelist = glob.glob(os.path.join(r'processed_imgs/', username + "*.png"))
-        if len(filelist) > 0:
-            for f in filelist:
-                os.remove(f)
-        # Create processes to start generating pictures
-        q_item = [username, twit.get_user_pic(username), twit.get_users_tweets(username)]
-        t = threading.Thread(name="ProducerThread", target=producer, args=(q1, q_item))
-        date_time = str(datetime.date.today()).replace('-', '_')
-        q2.put([username, date_time])
-        t.start()
-        return "Running video creater with final video at ec2-18-189-26-79.us-east-2.compute.amazonaws.com/video/twitter_feed_"+ username + '_' + date_time + '.mp4', 200
-    else:
-        return "Please enter a valid ID", 400
-
-
-# Function to play videos
-@app.route('/video/<name>')
-def play_video(name):
-    name = name.strip()
-    print(name)
-    if not os.path.isfile('./static/'+name):
-        return "Video is still processing", 400
-    html = f"""
-        <center>
-            <video width="768" controls>
-                <source src="/static/{name}" type="video/mp4">
-            </video>
-        </center>
-    """
-    return html, 200
 
 
 if __name__ == '__main__':
